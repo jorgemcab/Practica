a90ff7a5887b058f0255d710ee9a1f3c2bd259bd
unknown <jcabrera.jc75@gmail.com>
2024-07-07 14:11:46 -0300

    90% complete

--- main.js
+++ main.js
@@ -1,24 +1,71 @@
 const listaDeNombres = document.querySelector(".names-list");
+const heroForm = document.querySelector(".hero-form");
+const btnAcept = document.querySelector(".btn-acept");
+///////////////////inputs constantes///////////////
+const inputNombre = document.querySelector("#nombre");
+const inputApellido = document.querySelector("#apellido");
+const inputEdad = document.querySelector("#edad");
 
-let listaDeItems = [
-    {
-        Nombre: "jorgita",
-        Apellido: "boludita",
-        Edad: 15,
-        id: "aca va aide Date.now()"
-    }
-];
+
+let listaDeItems = JSON.parse(localStorage.getItem('items')) || [];
+
+const saveLocalStronati = ()=>{
+    localStorage.setItem("items", JSON.stringify(listaDeItems))
+};
 
 const render = () => {
     listaDeNombres.innerHTML = listaDeItems.map((indice) => {
-        return `Hola ${indice.Nombre} que cara de ${indice.Apellido}`
-    })
-    // `Hola ${listaDeItems[0].Nombre} que cara de ${listaDeItems[0].Apellido}`
+        return `<div class="contenedor-nombres">
+                    <li>Hola ${indice.Nombre} que cara de ${indice.Apellido}</li>
+                    <li> Edad: ${indice.Edad}
+                    <li class= "container-icon">
+                        <i class="fa-solid fa-user-xmark botonDeBorrar" data-id="${indice.id}" ></i>
+                    </li>
+                </div>`
+    }).join("")
+};
+
+const agregarItem = (e) => {
+    e.preventDefault();
+    if( inputNombre.value.length == 0 ){
+        return alert('ingresar nombre')
+    }
+    if( inputApellido.value.length == 0){
+        return alert("ingresar apellido")
+    }
+    if( inputEdad.value.length == 0){
+        return alert('ingresar edad')
+    }   else{
+        listaDeItems = [
+            ...listaDeItems,
+            {
+                Nombre: inputNombre.value,
+                Apellido: inputApellido.value,
+                Edad: inputEdad.value,
+                id: Date.now()
+            }]
+    };
+    render();
+    saveLocalStronati();
+    heroForm.reset()
 }
+
+const borrarItem = (e) => {
+    if(!e.target.classList.contains('botonDeBorrar')){
+        return
+    } else {
+        const filterId = Number( e.target.dataset.id);
+        listaDeItems = listaDeItems.filter((elem) => {
+            return elem.id != filterId
+        })};
+    render();
+    saveLocalStronati();
+};
 
 const init = () => {
     document.addEventListener("DOMContentLoaded", render);
-
+    heroForm.addEventListener("submit", agregarItem);
+    listaDeNombres.addEventListener("click", borrarItem);
 };
 
 init();
